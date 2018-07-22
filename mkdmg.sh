84aa5614e1d767c8ea1b20f737e9cac29bd6b8fb
Gene McCulley <mcculley@stackframe.com>
2018-07-22 15:37:11 -0400

    Remove in favor of Makefile.

--- mkdmg.sh
+++
@@ -1,9 +0,0 @@
-rm -fr tmp TMP.dmg "Print as PDF to clipboard.dmg"
-mkdir tmp
-cp -Rp Print\ as\ PDF\ to\ clipboard.app tmp/
-ln -s ~/Library/PDF\ Services/ tmp/
-cp *.rtf tmp/
-hdiutil makehybrid -hfs -hfs-volume-name "Print as PDF to clipboard" -hfs-openfolder tmp tmp -o TMP.dmg
-hdiutil convert -format UDZO TMP.dmg -o "Print as PDF to clipboard.dmg"
-rm TMP.dmg
-rm -fr tmp
--- mkpkg.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-pkgbuild --install-location /Library/PDF\ Services --sign "3rd Party Mac Developer Installer: StackFrame, LLC (5MRB2ZZE9C)" --component Print\ as\ PDF\ to\ clipboard.app/ Print\ as\ PDF\ to\ clipboard.pkg
